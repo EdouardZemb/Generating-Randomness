2a70120dc04154299223f30a47cbb2f01d4edf1c
Edouard Zemb <edouard.zemb@gmail.com>
2023-09-21 13:45:47 +0200

    refactor(stage 1): Add comments and remove code repetition

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -1,18 +1,37 @@
+# Define the minimum required length for the binary string
 MIN_LENGTH = 100
 
 
 def filter_binary(binary_string):
+    # Filter out characters that are not '0' or '1'
     return [x for x in binary_string if x in ["0", "1"]]
 
 
 def fetch_user_input():
+    # Get user input as a binary string
     return input("Print a random string containing 0 or 1:\n")
 
 
-binary_list = filter_binary(fetch_user_input())
+def main():
+    # Initialize an empty list to store the binary digits
+    binary_list = []
 
-while len(binary_list) < MIN_LENGTH:
-    print(f"Current data length is {str(len(binary_list))}, {str(MIN_LENGTH - len(binary_list))} symbols left")
-    binary_list += filter_binary(fetch_user_input())
+    # Continue fetching input until the list reaches the desired length
+    while len(binary_list) < MIN_LENGTH:
+        user_input = fetch_user_input()
+        filtered_input = filter_binary(user_input)
 
-print(f'Final data string:\n{"".join(binary_list)}')
+        # Extend the binary list with the filtered input
+        binary_list.extend(filtered_input)
+
+        # Calculate and display the remaining symbols needed
+        remaining_symbols = MIN_LENGTH - len(binary_list)
+        print(f"Current data length is {len(binary_list)}, {remaining_symbols} symbols left")
+
+    # Convert the binary list to a string and display the final result
+    final_data_string = "".join(binary_list)
+    print(f'Final data string:\n{final_data_string}')
+
+
+if __name__ == "__main__":
+    main()
