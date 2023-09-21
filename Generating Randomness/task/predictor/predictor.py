92ad1d6557ace91aeff1e30fa031d2ab567d9570
Edouard Zemb <edouard.zemb@gmail.com>
2023-09-21 14:24:00 +0200

    feat(stage 2): Counts different triads in final string and counts the number of 0 and 1 that follow them

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -12,6 +12,19 @@ def fetch_user_input():
     return input("Print a random string containing 0 or 1:\n")
 
 
+def calculate_triad_counts(binary_string):
+    triad_dict = {}
+
+    for i in range(len(binary_string) - 3):
+        triad = binary_string[i:i + 3]
+        if triad not in triad_dict:
+            triad_dict[triad] = {"0": 0, "1": 0}
+
+        triad_dict[triad][binary_string[i + 3]] += 1
+
+    return triad_dict
+
+
 def main():
     # Initialize an empty list to store the binary digits
     binary_list = []
@@ -31,6 +44,15 @@ def main():
     # Convert the binary list to a string and display the final result
     final_data_string = "".join(binary_list)
     print(f'Final data string:\n{final_data_string}')
+
+    # Calculate and display the triad counts
+    triad_dict = calculate_triad_counts(final_data_string)
+    triad_dict = {k: v for k, v in sorted(triad_dict.items(), key=lambda item: item[0])}
+
+    for k in triad_dict:
+        zero_count = triad_dict[k]["0"]
+        one_count = triad_dict[k]["1"]
+        print(f"{k}: {zero_count},{one_count}")
 
 
 if __name__ == "__main__":
